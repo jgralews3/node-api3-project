dca522e1e1eeb79d7e530c843dfe07d134c41b02
Joshua Gralewski <jgralews3@yahoo.com>
2020-10-10 05:59:10 -0500

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // code away!
+require('dotenv').config()
 const server = require('./server.js')
 
-server.listen(5000, () => {
-	console.log(`Server updated and running at http://localhost:5000`)
+const port = process.env.PORT;
+
+server.listen(port, () => {
+	console.log(`Server updated and running at http://localhost:${port}`)
 })
